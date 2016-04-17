10cc54230d0ccebcd3bafdb10f1b51e2dc94bd3d
mrcheng0910 <mrcheng0910@gmail.com>
2016-04-17 15:31:03 +0800

    添加二级顶级域名的分析后缀

--- analyse_sub_tld.py
+++ analyse_sub_tld.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # encoding:utf-8
 """
-统计分析domain的长度
+统计
 """
 
 from data_base import MySQL
 import numpy as np
 import matplotlib.pyplot as plt
-import  pandas as pd
+import pandas as pd
 
 
 def fetch_data():
@@ -16,11 +16,11 @@ def fetch_data():
     :return: 返回基础数据
     """
     db = MySQL()
-    sql = 'SELECT tld FROM domain_features WHERE tld_token_count = "2"'
+    sql = 'SELECT tld FROM domain_features WHERE tld_token_count = "2" limit 20000'
     db.query(sql)
-    domains_length = db.fetch_all_rows()
+    tlds = db.fetch_all_rows()
     db.close()
-    return domains_length
+    return tlds
 
 
 def create_data_array():
@@ -28,40 +28,71 @@ def create_data_array():
     根据获取的基础数据，建立对应的测试数据集合
     :return: 数据集合
     """
-    domains = fetch_data()  # domain基础数据
-    domain_length = []  # domain的长度
-    for line in domains:
-        domain_length.append(line[0])
-    domain_length = np.array(domain_length)
-    return domain_length
+    tlds = fetch_data()  # tld基础数据
+    entire_tlds = []  # 完整顶级后缀
+    first_tlds = []  # 顶级二级后缀
+    sub_tlds = []  # 次级二级后缀
+    for line in tlds:
+        entire_tlds.append(line[0])
+        sub_tlds.append(line[0].split('.')[0])
+        first_tlds.append(line[0].split('.')[1])
+
+    entire_tlds = np.array(entire_tlds)
+    sub_tlds = np.array(sub_tlds)
+    first_tlds = np.array(first_tlds)
+    return entire_tlds,sub_tlds,first_tlds
 
 
-def draw(domain_length):
+def draw(entire_tlds,sub_tlds,first_tlds):
     """
     绘制柱装图
-    :param domain_length:
+    :param entire_tlds:
     """
+    fig =plt.figure()
+
     x_label = []
-    x = pd.value_counts(pd.Series(domain_length)).index[:20]
-    y = pd.value_counts(pd.Series(domain_length)).values[:20]
+    x = pd.value_counts(pd.Series(entire_tlds)).index[:20]
+    y = pd.value_counts(pd.Series(entire_tlds)).values[:20]
     for label in x:
         x_label.append(str(label))
     x = np.arange(len(y))
-    fig =plt.figure()
-    ax = fig.add_subplot(121)
+
+    ax = fig.add_subplot(221)
     ax.bar(x,y)
     ax.set_xticks(x)
     ax.set_xticklabels(x_label,rotation=50)
     plt.grid()
-    ax2 = fig.add_subplot(122)
 
+    x_label = []
+    x = pd.value_counts(pd.Series(sub_tlds)).index[:20]
+    y = pd.value_counts(pd.Series(sub_tlds)).values[:20]
+    for label in x:
+        x_label.append(str(label))
+    x = np.arange(len(y))
+    ax2 = fig.add_subplot(222)
+    ax2.bar(x,y)
+    ax2.set_xticks(x)
+    ax2.set_xticklabels(x_label,rotation=50)
     plt.grid()
+
+    x_label = []
+    x = pd.value_counts(pd.Series(first_tlds)).index[:20]
+    y = pd.value_counts(pd.Series(first_tlds)).values[:20]
+    for label in x:
+        x_label.append(str(label))
+    x = np.arange(len(y))
+    ax3 = fig.add_subplot(223)
+    ax3.bar(x,y)
+    ax3.set_xticks(x)
+    ax3.set_xticklabels(x_label,rotation=50)
+    plt.grid()
+
     plt.show()
 
 
 def main():
-    df = create_data_array()
-    draw(df)
+    entire_tlds, sub_tlds, first_tlds = create_data_array()
+    draw(entire_tlds, sub_tlds,first_tlds)
 
 
 if __name__ == '__main__':
